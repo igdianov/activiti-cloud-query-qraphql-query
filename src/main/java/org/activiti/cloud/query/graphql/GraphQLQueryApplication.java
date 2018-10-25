87b0b0615ccf0aec1fe21a59aa824d536fb377f8
Igor Dianov <igor@dianov.com>
2018-10-24 22:18:43 -0700

    feat: add activiti-cloud-services-stagemonitor 7.0.0-SNAPSHOT

--- src/main/java/org/activiti/cloud/query/graphql/GraphQLQueryApplication.java
+++ src/main/java/org/activiti/cloud/query/graphql/GraphQLQueryApplication.java
@@ -2,14 +2,11 @@ package org.activiti.cloud.query.graphql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.stagemonitor.core.Stagemonitor;
 
 @SpringBootApplication
 public class GraphQLQueryApplication {
 
 	public static void main(String[] args) {
-        Stagemonitor.init();
-		
 		SpringApplication.run(GraphQLQueryApplication.class, args);
 	}
 }
